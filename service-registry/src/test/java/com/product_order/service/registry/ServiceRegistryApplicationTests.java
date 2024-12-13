196f8ec7e14ccd7a7c28be2df776ac45be0e7bc8
Naga Venkata Sairam Yanamadala <ynvsr.25@gmail.com>
2024-12-12 18:31:38 -0600

    added Registry

---
+++ service-registry/src/test/java/com/product_order/service/registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.product_order.service.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
